b6086424ff5397a969674eee7285e4de10262da6
Harun Gunes <h.gunes13@gmail.com>
2020-12-03 23:14:10 +0100

    Initialize repository

--- DiceGame/MainViewController.swift
+++ DiceGame/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  DiceGame
 //
 //  Created by Harun Gunes on 03/12/2020.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MainViewController: UIViewController {
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    
   }
-
-
 }
-
